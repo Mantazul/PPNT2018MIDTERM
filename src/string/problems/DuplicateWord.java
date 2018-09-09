730ae09cec6a72bedaeb8ed06a3c7ee721269ce7
Juned Alam <juned5252@gmail.com>
2018-09-09 13:32:14 -0400

    added DuplicateWord Solution

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,9 +1,31 @@
 package string.problems;
 
+import sun.applet.resources.MsgAppletViewer;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class DuplicateWord {
+
+    public static  void DuplicateWords(String str){
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        String[] arr = str.split(" ");
+
+        for (String x: arr) {
+            if (map.get(x) != null){
+                map.put(x,map.get(x)+1);
+            }else {
+                map.put(x,1);
+            }
+        }
+
+        for (Map.Entry entry: map.entrySet()) {
+            System.out.println(entry.getKey() +": "+entry.getValue());
+        }
+    }
 
     public static void main(String[] args) {
         /*
@@ -12,6 +34,7 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        DuplicateWords(st);
 
     }
 
